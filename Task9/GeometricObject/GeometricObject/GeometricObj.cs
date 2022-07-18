d00bb5894fee0e6c31de1ee7b67ba44f1078270b
Minh Tuấn <khanhdo601@gmail.com>
2022-07-18 16:46:17 +0700

    Task9
    ok

---
+++ Task9/GeometricObject/GeometricObject/GeometricObj.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+public abstract class GeometricObject
+{
+    protected double weight;
+    protected string color;
+    protected GeometricObject()
+    {
+        color = "white";
+        weight = 1.0;
+    }
+    protected GeometricObject(double weight, string color)
+    {
+        this.weight = weight;
+        this.color = color;
+    }
+    public string PColor
+    {
+        get { return this.color; }
+        set { this.color = value; }
+    }
+    public double PWeight
+    {
+        get { return this.weight; }
+        set { this.weight = value; }
+    }
+    public abstract double findArea();
+    public abstract double findPerimeter();
+}
